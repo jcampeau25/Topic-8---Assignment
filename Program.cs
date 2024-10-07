bc96c987f17489112ada7b40652638aa43f39338
jcampeau25 <jdccanuck25@gmail.com>
2024-10-07 08:34:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Topic_8___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading in Colours
+            List <string> colours = new List<string> { "Red", "Orange", "Yellow", "Green", "Blue", "Purple" };
+            Random generator = new Random();
+            int colour = generator.Next(colours.Count);
+            Console.WriteLine(colours[colour]);
+
+
+            // Random Numbers
+        }
+    }
+}
